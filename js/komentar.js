cd70e9b9ef73b866919cdaa7b78d14caafea4f8b
Ignasiussigit <martixdna@gmail.com>
2022-04-20 15:31:20 +0700

    menambahkan komentar

---
+++ js/komentar.js
@@ -0,0 +1,104 @@
+// Add note to local storage
+let addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click", function (e) {
+
+    let addTitle = document.getElementById("note-title");
+    let addTxt = document.getElementById("note-text");
+
+    if (addTitle.value == "" || addTxt.value == "") {
+        return alert("Silakan tambahkan Nama anda")
+    }
+
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    let myObj = {
+        title: addTitle.value,
+        text: addTxt.value
+    }
+    notesObj.push(myObj);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    addTitle.value = "";
+    //   console.log(notesObj);
+    showNotes();
+});
+
+// Function to show elements from localStorage
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class="note">
+            <p class="note-counter">Komentar ${index + 1}</p>
+            <h3 class="note-title"> ${element.title} </h3>
+            <p class="note-text"> ${element.text}</p>
+            <button id="${index}"onclick="deleteNote(this.id)" class="note-btn">Hapus Komentar</button>
+            <button id="${index}"onclick="editNote(this.id)" class="note-btn edit-btn">Edit Komentar</button>
+        </div>
+            `;
+    });
+    let notesElm = document.getElementById("notes");
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    } else {
+        notesElm.innerHTML = `Belum ada Komentar! Tambahkan Komentar menggunakan formulir di atas..`;
+    }
+}
+
+// Function to delete a note
+function deleteNote(index) {
+    //   console.log("I am deleting", index);
+    let confirmDel = confirm("Kamu Yakin ingin menghapus Komentar?");
+    if (confirmDel == true) {
+        let notes = localStorage.getItem("notes");
+        if (notes == null) {
+            notesObj = [];
+        } else {
+            notesObj = JSON.parse(notes);
+        }
+
+        notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+    }
+
+}
+
+// Function to Edit the Note
+function editNote(index) {
+    let notes = localStorage.getItem("notes");
+    let addTitle = document.getElementById("note-title");
+    let addTxt = document.getElementById("note-text");
+
+    if (addTitle.value !== "" || addTxt.value !== "") {
+        return alert("Harap kosongkan formulir sebelum mengedit catatan")
+    }
+
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    console.log(notesObj);
+
+    notesObj.findIndex((element, index) => {
+        addTitle.value = element.title;
+        addTxt.value = element.text;
+    })
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+}
+
+
+showNotes();
